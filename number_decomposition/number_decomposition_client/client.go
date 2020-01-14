6fd5808f6239fcee4dbb4b4c14bf391bf3632c1e
TANMAYA <vibhorgupta2008@aol.in>
2020-01-14 18:51:36 +0530

    Add client code implementation of number decomposition into primes

---
+++ number_decomposition/number_decomposition_client/client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/VIBHOR94/grpc/number_decomposition/number_decompositionpb"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connetc %v", err)
+	}
+	defer cc.Close()
+
+	c := number_decompositionpb.NewNumberDecompositionServiceClient(cc)
+	doServerStreaming(c)
+}
+
+func doServerStreaming(c number_decompositionpb.NumberDecompositionServiceClient) {
+	req := &number_decompositionpb.NumberDecompositionRequest{
+		Number: 120,
+	}
+	resStream, err := c.Decomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Decomposition RPC: %v", c)
+	}
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while reading stream %v", err)
+		}
+		log.Printf("Response from Decomposition: %v", msg)
+	}
+}
