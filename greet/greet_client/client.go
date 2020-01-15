8817e0c64fb52928ddd5af66778be32a80ac4885
TANMAYA <vibhorgupta2008@aol.in>
2020-01-15 20:16:38 +0530

    Add client side code for implementing GreeEveryOne(BiDi Streaming) RPC call

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -25,8 +25,8 @@ func main() {
 	// fmt.Printf("Created client: %f", c)
 	// doUnary(c)
 	// doServerStreaming(c)
-	doClientStreaming(c)
-
+	// doClientStreaming(c)
+	doBiDiStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -106,4 +106,65 @@ func doClientStreaming(c greetpb.GreetServiceClient) {
 		log.Fatalf("Error while receiving response from Longgreet: %v", err)
 	}
 	fmt.Printf("LongGreet Response: %v\n", res)
+}
+
+func doBiDiStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Do a BiDi streaming")
+	requests := []*greetpb.GreetEveryOneRequest{
+		&greetpb.GreetEveryOneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Stephane",
+			},
+		},
+		&greetpb.GreetEveryOneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "John",
+			},
+		},
+		&greetpb.GreetEveryOneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Mark",
+			},
+		},
+		&greetpb.GreetEveryOneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Piper",
+			},
+		},
+	}
+	waitc := make(chan struct{})
+
+	// we create a stream by invoking the client
+	stream, err := c.GreetEveryOne(context.Background())
+	if err != nil {
+		log.Fatalf("error while calling GreetEveryOne: %v", err)
+	}
+
+	// we send a bunch of messages to the client (go routine)
+	go func() {
+		for _, req := range requests {
+			fmt.Printf("Sending req %v\n", req)
+			stream.Send(req)
+			time.Sleep(1000 * time.Millisecond)
+		}
+		stream.CloseSend()
+	}()
+
+	// we send a bunch of responses (go routine)
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error while receiving: %v", err)
+				break
+			}
+			fmt.Printf("Received: %v\n", res)
+		}
+		close(waitc)
+	}()
+
+	<-waitc
 }
